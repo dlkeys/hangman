f7ae4c7099fa1b15784ffcb47047320495565a94
David Keys <dlkeys@yahoo.com>
2018-01-25 15:33:40 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,18 +1,19 @@
 import time
 
 # Personalize the game with the players name
+
 name = input("What is your name? ")
-print ("Hello, " + name, "Time to play hangman!")
+print ("Hello " + name+", time to play hangman!")
 print (" ")
-time.sleep(3)
+time.sleep(2)
 print ("Start guessing...")
 time.sleep(0.5)
 
 # For now, I will only have one word to guess
 # Later, we will expand this greatly
-word = "david"
-
 # Initialize the variables
+
+word = "christmas"
 guesses = ''
 turns = 10
 
@@ -20,25 +21,25 @@ while turns > 0:
     failed = 0
     for char in word:      
         if char in guesses:    
-            print (char),
+            print (char)
         else:    
-            print ("_"),   
+            print ("_")
             failed += 1    
     if failed == 0:        
-        print ("You won")		
+        print ("You win!!!")		
         break              
 
-    print
-
-    guess = input("guess a character:") 
+    time.sleep(0.5)
+    print (" ")
+    guess = input("Guess a character:") 
     guesses += guess                    
-
+    print ("Letters used: ", guesses)
+    if guess in word:
+      print ("Right")
+      print ("You have", turns, "more guesses")
     if guess not in word:  
         turns -= 1        
-        print ("Wrong")  
+        print ("Wrong")
+        print ("You have", turns, 'more guesses')
  
-        print ("You have", + turns, 'more guesses')
- 
-        if turns == 0:           
-    
-            print ("You Lose")
+        if turns == 0:
