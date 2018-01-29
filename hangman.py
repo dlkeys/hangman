278aa86915f80870f26306b949d3a4fc4ea86a66
David Keys <dlkeys@yahoo.com>
2018-01-29 14:43:04 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,45 +1,45 @@
 import time
-
-# Personalize the game with the players name
+import secrets
 
 name = input("What is your name? ")
-print ("Hello " + name+", time to play hangman!")
 print (" ")
-time.sleep(2)
+print ("Hello " + name+ ", time to play hangman!")
+print (" ")
+time.sleep(1.5)
 print ("Start guessing...")
 time.sleep(0.5)
 
-# For now, I will only have one word to guess
-# Later, we will expand this greatly
-# Initialize the variables
-
-word = "christmas"
+words = ['christmas', 'easter', 'thanksgiving', 'halloween', 'valentines']
+word = secrets.choice(words)
 guesses = ''
 turns = 10
 
 while turns > 0:
     failed = 0
-    for char in word:      
-        if char in guesses:    
-            print (char)
-        else:    
+    for letter in word:
+        if letter in guesses:
+            print (letter)
+        else:
             print ("_")
-            failed += 1    
-    if failed == 0:        
-        print ("You win!!!")		
-        break              
+            failed += 1
+    if failed == 0:
+        print ("You win!!!")
+        break
 
     time.sleep(0.5)
     print (" ")
-    guess = input("Guess a character:") 
-    guesses += guess                    
+    guess = input("Guess a character:")
+    guesses += guess
     print ("Letters used: ", guesses)
     if guess in word:
-      print ("Right")
+      print ("Correct")
       print ("You have", turns, "more guesses")
-    if guess not in word:  
-        turns -= 1        
-        print ("Wrong")
+      print (" ")
+    if guess not in word:
+        turns -= 1
+        print ("Incorrect")
         print ("You have", turns, 'more guesses')
- 
+        print (" ")
         if turns == 0:
+          print ("You Lose!!!")
+          break
